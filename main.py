df3f3a2aed1ce6bc808de059e3b2b52f26b9c46d
marcelosimim <54868944+marcelosimim@users.noreply.github.com>
2021-08-10 13:36:00 -0300

    bubble

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from array_generator import *
+from sort_methods import *
 
 i = 100
 j = 1000
 k = 10000
 n = 1000000
 
-print(ordered_array(i))
-print(disordered_array(i))
-print(random_array(i))
-print(partially_random_array(i))
+print(bubble(partially_random_array(i)))
--- sort_methods.py
+++ sort_methods.py
@@ -1,5 +1,11 @@
-def bubble(n):
-    print(n)
+def bubble(array):
+    for i in range(len(array)-1, 0, -1):
+        for j in range(i):
+            if array[j] > array[j+1]:
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+    return array
 
 
 def insert(n):
